914340e0ac051b775420d6b3d7bbce1092132252
vikash <niteshbisht98@gmail.com>
2016-03-05 12:14:30 +0530

    first service hit

--- challengeapp/src/main/java/com/app/challenge/event/service/EventService.java
+++ challengeapp/src/main/java/com/app/challenge/event/service/EventService.java
@@ -1,5 +1,19 @@
 package com.app.challenge.event.service;
 
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+@Path("/rivalService")
 public interface EventService {
 
+	
+	@GET
+	@Produces({MediaType.APPLICATION_XML,MediaType.APPLICATION_JSON})
+	@Path("/registerNewUser")
+	public Response registerNewUser(String token, String email); 
+	
+	
 }
--- challengeapp/src/main/java/com/app/challenge/event/service/EventServiceImpl.java
+++ challengeapp/src/main/java/com/app/challenge/event/service/EventServiceImpl.java
@@ -1,5 +1,20 @@
 package com.app.challenge.event.service;
 
+import javax.ws.rs.core.Response;
+
+import org.springframework.stereotype.Service;
+
+
+@Service("rivalService")
 public class EventServiceImpl implements EventService {
+	
+	
+	@Override
+	public Response registerNewUser(String token, String email) {
+		
+		return Response.ok("registered").build();
+	}
+
+	
 
 }
